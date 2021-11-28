82e3caa0d6b9d0d1b985ef2b95dd16fc27ad1ea2
tejaskelkar <tejaskelkar@gmail.com>
2021-11-28 13:00:07 -0500

    add modal component and overlay functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 
 function App() {
+  const [cartShown, setCartShown] = useState(false);
+
+  const shownCartHandler = () => {
+    setCartShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartShown(false);
+  }
+
   return (
     <Fragment>
-      <Header></Header>
+      {cartShown && <Cart onHideCart={hideCartHandler}/>}
+      <Header onShowCart={shownCartHandler} />
       <main>
         <Meals/>
       </main>
